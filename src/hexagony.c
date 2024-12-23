748de2e8d7fea9368a91404ab7abee4278dcbeb3
DubiousDoggo <jft360@gmail.com>
2024-12-22 18:39:10 -0600

    bugfixes

--- src/hexagony.c
+++ src/hexagony.c
@@ -23,12 +23,16 @@ struct program_cell {
 };
 
 // Memory is defined as an infinite hexagonal grid where each egde is a value.
-// In this implementation, memory is indexed with the axial coordinates of an offset hexagonal grid, corresponding to a
-// cubic offset of z+1. Each hexagon in this offset grid stores 3 values, one for each cubic axis, corresponding to the
-// edges of the original grid. Since the memory is theoretically infinite, allocation grows outward in rings as needed.
-// Storing each ring sequentially reduces memory overhead at the cost of a bit of math.
+// see https://www.redblobgames.com/grids/hexagons/ for terminology
+
+// In this implementation, memory is indexed with the axial coordinates of a
+// hexagonal grid. Each hexagon in the grid stores 3 values, one for each cubic
+// axis.
+
+typedef int memory_edge;
+
 struct memory_cell {
-    int value[3];
+    memory_edge value[3];
 };
 
 struct memory_pointer {
@@ -37,7 +41,7 @@ struct memory_pointer {
     enum { IN, OUT } direction;
 };
 
-// x,y axial offests for each hexagonal direction
+// axial offests for each hexagonal direction
 const struct {
     long dp, dq;
 } direction_offset[] = {
@@ -68,7 +72,7 @@ ssize_t axial_to_index(long p, long q, long rings) {
     return i;
 }
 
-// Convert x,y axial coordinated to a radial index, see memory_cell
+// Convert x,y axial coordinate to a radial index
 size_t axial_to_mem_index(long p, long q) {
     long x = p;
     long y = q;
@@ -93,6 +97,7 @@ size_t axial_to_mem_index(long p, long q) {
     return i;
 }
 
+// memory is allocated sequentially and grows outwards in rings
 struct memory_cell *realloc_memory(struct memory_cell *memory, long old_rings, long new_rings) {
     size_t old_size = (3 * old_rings * (old_rings - 1) + 1);
     size_t new_size = (3 * new_rings * (new_rings - 1) + 1);
@@ -115,11 +120,12 @@ struct memory_cell *get_memory_cell(long p, long q, struct memory_cell **memory,
     return *memory + index;
 }
 
-int *get_memory_edge(struct memory_pointer ptr, struct memory_cell **memory, long *rings) {
+// get a pointer to the memory edge pointed to by ptr
+memory_edge *get_memory_edge(struct memory_pointer ptr, struct memory_cell **memory, long *rings) {
     return &get_memory_cell(ptr.p, ptr.q, memory, rings)->value[ptr.axis];
 }
 
-int *get_neighbor(struct memory_pointer ptr, enum neighbor neighbor, struct memory_cell **memory, long *rings) {
+memory_edge *get_neighbor(struct memory_pointer ptr, enum neighbor neighbor, struct memory_cell **memory, long *rings) {
     struct memory_cell *cell;
     long xyz[3] = {ptr.p, ptr.q, -ptr.p - ptr.q};
     if (ptr.direction == OUT) {
@@ -206,29 +212,46 @@ void print_program(struct program_cell *program, long program_rings, ssize_t ip_
     }
 }
 
-void print_memory(struct memory_cell *memory, long rings) {
+void print_memory(struct memory_cell *memory, long rings, const struct memory_pointer *ptr) {
 
-    for (long z = rings - 1; z >= 1 - rings; z--) {
-        long x = rings - 1;
-        long y = 1 - rings;
+    const long print_rings = 4;
+    const struct memory_cell oob = {0, 0, 0};
+    const long ptr_z = -ptr->p - ptr->q;
+    printf("[%ld rings allocated]\n", rings);
+
+    for (long z = print_rings; z >= -print_rings; z--) {
+
+        long x = print_rings;
+        long y = -print_rings;
         if (z > 0)
             x -= z;
         if (z < 0)
             y -= z;
 
-        for (long s = 0; s < abs(z); s++)
+        for (long s = 0; s < labs(z); s++)
             printf("  %*s ", MEM_FMT_LEN, "");
-        for (long p = x, q = y; abs(p) + abs(q) + abs(z) <= rings; --p, q++) {
-            struct memory_cell *cell = get_memory_cell(p, q, &memory, &rings);
-            printf("    " MEM_FMT " %*s ", cell->value[Z], MEM_FMT_LEN, "");
+        for (long p = x, q = y; labs(p) + labs(q) + labs(z) <= 2 * print_rings; --p, q++) {
+            const struct memory_cell *cell;
+            if (labs(ptr->p + p) + labs(ptr->q + q) + labs(ptr_z + z) <= 2 * rings)
+                cell = get_memory_cell(ptr->p + p, ptr->q + q, &memory, &rings);
+            else
+                cell = &oob;
+            printf("    \e[0;3%dm" MEM_FMT "\e[0m %*s ", (p == 0 && q == 0 && ptr->axis == Z) ? 1 : 0, cell->value[Z],
+                   MEM_FMT_LEN, "");
         }
         putchar('\n');
 
-        for (long s = 0; s < abs(z); s++)
+        for (long s = 0; s < labs(z); s++)
             printf("  %*s ", MEM_FMT_LEN, "");
-        for (long p = x, q = y; abs(p) + abs(q) + abs(z) <= rings; --p, q++) {
-            struct memory_cell *cell = get_memory_cell(p, q, &memory, &rings);
-            printf(". " MEM_FMT " ' " MEM_FMT " ", cell->value[X], cell->value[Y]);
+        for (long p = x, q = y; labs(p) + labs(q) + labs(z) <= 2 * print_rings; --p, q++) {
+            const struct memory_cell *cell;
+            if (labs(ptr->p + p) + labs(ptr->q + q) + labs(ptr_z + z) <= 2 * rings)
+                cell = get_memory_cell(ptr->p + p, ptr->q + q, &memory, &rings);
+            else
+                cell = &oob;
+            printf(". \e[0;3%dm" MEM_FMT "\e[0m ' \e[0;3%dm" MEM_FMT "\e[0m ",
+                   (p == 0 && q == 0 && ptr->axis == X) ? 1 : 0, cell->value[X],
+                   (p == 0 && q == 0 && ptr->axis == Y) ? 1 : 0, cell->value[Y]);
         }
         puts(".");
     }
@@ -290,7 +313,7 @@ int main(int argc, char **argv) {
 
     long memory_rings = 1;
     struct memory_cell *memory = calloc(1, sizeof(struct memory_cell));
-    struct memory_pointer MP = {0, 0, Z, IN};
+    struct memory_pointer MP = {0, 0, Z, OUT};
 
     bool force_debug = false;
     struct program_cell *instruction;
@@ -301,6 +324,8 @@ int main(int argc, char **argv) {
         } else {
             instruction = program + axial_to_index(IP->p, IP->q, program_rings);
             if (instruction->debug || force_debug) {
+                if (instruction->debug)
+                    puts("break");
                 printf("\nPaused on '%c'\n", instruction->value);
                 ssize_t ips[6];
                 for (unsigned ip = 0; ip < 6; ip++)
@@ -311,14 +336,16 @@ int main(int argc, char **argv) {
                 for (int i = 0; i < 6; i++)
                     printf("IP \e[0;3%dm%d\e[0m (%+*ld, %+*ld) %s\n", i + 1, i, digits, IPs[i].p, digits, IPs[i].q,
                            direction_name[IPs[i].direction]);
-                print_memory(memory, memory_rings);
-                printf("MP: (%+ld, %+ld) %s %s\n", MP.p, MP.q, axis_name[MP.axis],
-                       MP.direction == IN ? "INWARDS" : "OUTWARDS");
+                print_memory(memory, memory_rings, &MP);
+                printf("MP: (%+ld, %+ld) %s %s = " MEM_FMT "\n", MP.p, MP.q, axis_name[MP.axis],
+                       MP.direction == IN ? "INWARDS" : "OUTWARDS", *get_memory_edge(MP, &memory, &memory_rings));
+            prompt:
                 printf(": ");
                 switch (getchar()) {
                 case 's': force_debug = true; break;
                 case 'c': force_debug = false; break;
                 case 'q': goto done;
+                default: goto prompt;
                 }
             }
             if (isalpha(instruction->value)) // set MP to value
@@ -328,8 +355,8 @@ int main(int argc, char **argv) {
                 // negative value, the digit is subtracted instead of added.
                 int *edge = get_memory_edge(MP, &memory, &memory_rings);
                 *edge *= 10;
-                *edge += (*edge < 0 ? -1 : 1) * instruction->value - '0';
-            } else {
+                *edge += (*edge < 0 ? -1 : 1) * (instruction->value - '0');
+            } else
                 switch (instruction->value) {
                 // no-op.
                 case '.': break;
@@ -378,7 +405,9 @@ int main(int argc, char **argv) {
 
                 // reads a single byte from STDIN and sets the current memory edge to its value, or -1 if EOF
                 // reached.
-                case ',': *get_memory_edge(MP, &memory, &memory_rings) = getchar(); break;
+                case ',': {
+                    *get_memory_edge(MP, &memory, &memory_rings) = getchar();
+                } break;
 
                 // reads and discards from STDIN until a digit, a - or a + is found. Then reads as many characters
                 // as possible to form a valid (signed) decimal integer and sets the current memory edge to its
@@ -490,7 +519,7 @@ int main(int argc, char **argv) {
                 // switches to the next IP
                 case ']': IP_index = modulo(IP_index + 1, 6); break;
                 // takes the current memory edge modulo 6 and switches to the IP with that index.
-                case '#': IP_index = modulo(*get_memory_edge(MP, &memory, &memory_rings), 6);
+                case '#': IP_index = modulo(*get_memory_edge(MP, &memory, &memory_rings), 6); break;
 
                 // moves the MP to the left neighbour.
                 case '{': move_mp(&MP, LEFT); break;
@@ -524,7 +553,6 @@ int main(int argc, char **argv) {
                     *edge = *get_neighbor(MP, *edge <= 0 ? LEFT : RIGHT, &memory, &memory_rings);
                 } break;
                 }
-            }
         }
         long np = IP->p + direction_offset[IP->direction].dp;
         long nq = IP->q + direction_offset[IP->direction].dq;
